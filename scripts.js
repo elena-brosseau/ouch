7fee86673454809340858c19f8965863ab2019f1
HelloGonzo <118839950+HelloGonzo@users.noreply.github.com>
2023-02-27 19:53:13 -0500

    updated all thumbnails

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 
+
+/*
 const anaVid = document.querySelector('#ANA-vid');
 
 anaVid.onclick = () => {
@@ -8,9 +10,6 @@ anaVid.onclick = () => {
     anaVid.controls = true;
 }
 
-
-
-
 function startPreview(video) {
     video.muted = true;
     video.currentTime = 132;
@@ -19,3 +18,30 @@ function startPreview(video) {
   
 startPreview(anaVid)
 let anaLoop = setInterval(() => {startPreview(anaVid)}, 5900)
+*/
+
+
+const contact = document.querySelector('#contact'),
+      email = document.querySelector('#contact p')
+      copy = document.querySelector('#contact img');
+
+contact.addEventListener('click', openContact)
+
+function openContact() {
+  contact.style.maxWidth = '260px';
+  email.innerHTML = 'sophie@ouchproductions.tv';
+  copy.style.display = 'block';
+  contact.removeEventListener('click', openContact);
+}
+
+function closeContact() {
+  contact.style.maxWidth = '160px';
+  email.innerHTML = 'let\'s work together';
+  copy.style.display = 'none';
+  contact.addEventListener('click', openContact);
+}
+
+copy.onclick = () => {
+  navigator.clipboard.writeText(email.innerHTML);
+  closeContact();
+}
